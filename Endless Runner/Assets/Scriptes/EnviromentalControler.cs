17346ce3cbab591da18b853dd52be3bcae2dcd9c
EtMingerink <ethanmingerink@yahoo.com>
2024-10-24 12:11:35 -0400

    Begining of module 10 2

--- Endless Runner/Assets/Scriptes/EnviromentalControler.cs
+++ Endless Runner/Assets/Scriptes/EnviromentalControler.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class EnviromentalControler : MonoBehaviour
 {
-
-
+    [SerializeField] GameObject environmentElement;
+    [SerializeField] Transform referencePoint;
 
     
     void Start()
     {
-        Debug.Log(SumOfTwoNumbers(4, 9));
+        //Debug.Log(SumOfTwoNumbers(4, 9));
+        StartCoroutine(CreateEnvironmentElement());
     }
 
 
 
-    int SumOfTwoNumbers(int number1, int number2)
+    /*int SumOfTwoNumbers(int number1, int number2)
     {
         return number1 + number2;
+    }*/
 
-
-
+    IEnumerator CreateEnvironmentElement()
+    {
+        if (environmentElement != null && referencePoint != null)
+        {
+            Instantiate(environmentElement, referencePoint.position, Quaternion.identity);
+            yield return new WaitForSeconds(3);
+            StartCoroutine(CreateEnvironmentElement());
+        }
+        else
+        {
+            Debug.LogError("environmentElement or referencePoint is not assigned!");
+        }
     }
 }
+
